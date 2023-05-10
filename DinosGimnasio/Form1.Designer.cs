c296c103ca72c4b3bba5b8c0493f9f938c39a93d
Carlo Palladino”git config --global user.name CarlosPalladino” <carlosezequiel19palladino@gmail.com>
2023-05-10 10:42:29 -0300

    agregando clases y nuevas vistas

--- DinosGimnasio/Form1.Designer.cs
+++ DinosGimnasio/Form1.Designer.cs
@@ -29,63 +29,60 @@
         private void InitializeComponent()
         {
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.PanelMenu = new System.Windows.Forms.Panel();
-            this.PanelLog = new System.Windows.Forms.Panel();
             this.BtnLogin = new System.Windows.Forms.Button();
+            this.PanelMenu = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.PanelMenu.SuspendLayout();
-            this.PanelLog.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(19, 1);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(51, 21);
-            this.textBox1.TabIndex = 0;
-            // 
-            // PanelMenu
-            // 
-            this.PanelMenu.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.PanelMenu.Controls.Add(this.PanelLog);
-            this.PanelMenu.Dock = System.Windows.Forms.DockStyle.Left;
-            this.PanelMenu.Location = new System.Drawing.Point(7, 7);
-            this.PanelMenu.Name = "PanelMenu";
-            this.PanelMenu.Size = new System.Drawing.Size(256, 511);
-            this.PanelMenu.TabIndex = 1;
-            // 
-            // PanelLog
-            // 
-            this.PanelLog.BackColor = System.Drawing.Color.DarkSlateGray;
-            this.PanelLog.Controls.Add(this.BtnLogin);
-            this.PanelLog.Dock = System.Windows.Forms.DockStyle.Top;
-            this.PanelLog.Location = new System.Drawing.Point(0, 0);
-            this.PanelLog.Name = "PanelLog";
-            this.PanelLog.Size = new System.Drawing.Size(256, 251);
-            this.PanelLog.TabIndex = 0;
             // 
             // BtnLogin
             // 
             this.BtnLogin.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BtnLogin.BackColor = System.Drawing.Color.DarkSlateGray;
-            this.BtnLogin.Dock = System.Windows.Forms.DockStyle.Left;
             this.BtnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnLogin.Font = new System.Drawing.Font("Lucida Sans Typewriter", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnLogin.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.BtnLogin.Location = new System.Drawing.Point(0, 0);
+            this.BtnLogin.Location = new System.Drawing.Point(113, 231);
             this.BtnLogin.Name = "BtnLogin";
-            this.BtnLogin.Size = new System.Drawing.Size(256, 251);
+            this.BtnLogin.Size = new System.Drawing.Size(198, 39);
             this.BtnLogin.TabIndex = 0;
             this.BtnLogin.Text = "Ingresá";
             this.BtnLogin.UseVisualStyleBackColor = false;
             this.BtnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
             // 
+            // PanelMenu
+            // 
+            this.PanelMenu.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.PanelMenu.Controls.Add(this.textBox3);
+            this.PanelMenu.Controls.Add(this.textBox2);
+            this.PanelMenu.Controls.Add(this.BtnLogin);
+            this.PanelMenu.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.PanelMenu.Location = new System.Drawing.Point(238, 123);
+            this.PanelMenu.Name = "PanelMenu";
+            this.PanelMenu.Size = new System.Drawing.Size(432, 308);
+            this.PanelMenu.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(113, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 21);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(113, 150);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(200, 21);
+            this.textBox3.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.ClientSize = new System.Drawing.Size(895, 525);
             this.Controls.Add(this.PanelMenu);
-            this.Controls.Add(this.textBox1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.SystemColors.GrayText;
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
@@ -93,19 +90,18 @@
             this.Padding = new System.Windows.Forms.Padding(7);
             this.Text = "Form1";
             this.PanelMenu.ResumeLayout(false);
-            this.PanelLog.ResumeLayout(false);
+            this.PanelMenu.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Panel PanelMenu;
-        private System.Windows.Forms.Panel PanelLog;
         private System.Windows.Forms.Button BtnLogin;
+        private System.Windows.Forms.Panel PanelMenu;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- DinosGimnasio/FormMenu.Designer.cs
+++ DinosGimnasio/FormMenu.Designer.cs
@@ -29,71 +29,173 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.lblMensualidad = new System.Windows.Forms.Label();
+            this.lblDni = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
             this.lblClave = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApelido = new System.Windows.Forms.TextBox();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.txtImagen = new System.Windows.Forms.Label();
+            this.pickImagen = new System.Windows.Forms.PictureBox();
+            this.btnAgregarImagen = new System.Windows.Forms.Button();
+            this.cboMensualidad = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pickImagen)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.cboMensualidad);
+            this.panel1.Controls.Add(this.btnAgregarImagen);
+            this.panel1.Controls.Add(this.pickImagen);
+            this.panel1.Controls.Add(this.txtImagen);
+            this.panel1.Controls.Add(this.btnAceptar);
+            this.panel1.Controls.Add(this.BtnReset);
+            this.panel1.Controls.Add(this.txtDni);
+            this.panel1.Controls.Add(this.txtApelido);
+            this.panel1.Controls.Add(this.txtNombre);
+            this.panel1.Controls.Add(this.txtClave);
+            this.panel1.Controls.Add(this.lblMensualidad);
+            this.panel1.Controls.Add(this.lblDni);
+            this.panel1.Controls.Add(this.lblApellido);
+            this.panel1.Controls.Add(this.lblNombre);
             this.panel1.Controls.Add(this.lblClave);
             this.panel1.Location = new System.Drawing.Point(46, 29);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(705, 369);
             this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // lblMensualidad
+            // 
+            this.lblMensualidad.AutoSize = true;
+            this.lblMensualidad.Location = new System.Drawing.Point(13, 225);
+            this.lblMensualidad.Name = "lblMensualidad";
+            this.lblMensualidad.Size = new System.Drawing.Size(67, 13);
+            this.lblMensualidad.TabIndex = 4;
+            this.lblMensualidad.Text = "Mensualidad";
+            // 
+            // lblDni
+            // 
+            this.lblDni.AutoSize = true;
+            this.lblDni.Location = new System.Drawing.Point(13, 180);
+            this.lblDni.Name = "lblDni";
+            this.lblDni.Size = new System.Drawing.Size(35, 13);
+            this.lblDni.TabIndex = 3;
+            this.lblDni.Text = "D.N. I";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(13, 130);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(44, 13);
+            this.lblApellido.TabIndex = 2;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(13, 86);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre";
             // 
             // lblClave
             // 
             this.lblClave.AutoSize = true;
-            this.lblClave.Location = new System.Drawing.Point(29, 61);
+            this.lblClave.Location = new System.Drawing.Point(13, 39);
             this.lblClave.Name = "lblClave";
             this.lblClave.Size = new System.Drawing.Size(34, 13);
             this.lblClave.TabIndex = 0;
             this.lblClave.Text = "Clave";
             // 
-            // label2
+            // txtClave
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 108);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.txtClave.Location = new System.Drawing.Point(97, 36);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.Size = new System.Drawing.Size(114, 20);
+            this.txtClave.TabIndex = 5;
             // 
-            // label3
+            // txtNombre
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(29, 152);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.txtNombre.Location = new System.Drawing.Point(97, 83);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(114, 20);
+            this.txtNombre.TabIndex = 6;
             // 
-            // label4
+            // txtApelido
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(29, 202);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "label4";
+            this.txtApelido.Location = new System.Drawing.Point(97, 127);
+            this.txtApelido.Name = "txtApelido";
+            this.txtApelido.Size = new System.Drawing.Size(114, 20);
+            this.txtApelido.TabIndex = 7;
             // 
-            // label5
+            // txtDni
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(29, 249);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "label5";
+            this.txtDni.Location = new System.Drawing.Point(97, 177);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(114, 20);
+            this.txtDni.TabIndex = 8;
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.Location = new System.Drawing.Point(231, 324);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(95, 22);
+            this.BtnReset.TabIndex = 10;
+            this.BtnReset.Text = "Cancelar";
+            this.BtnReset.UseVisualStyleBackColor = true;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(361, 324);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(95, 22);
+            this.btnAceptar.TabIndex = 11;
+            this.btnAceptar.Text = "Agregar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // txtImagen
+            // 
+            this.txtImagen.AutoSize = true;
+            this.txtImagen.Location = new System.Drawing.Point(314, 147);
+            this.txtImagen.Name = "txtImagen";
+            this.txtImagen.Size = new System.Drawing.Size(42, 13);
+            this.txtImagen.TabIndex = 12;
+            this.txtImagen.Text = "Imagen";
+            // 
+            // pickImagen
+            // 
+            this.pickImagen.Location = new System.Drawing.Point(415, 36);
+            this.pickImagen.Name = "pickImagen";
+            this.pickImagen.Size = new System.Drawing.Size(227, 202);
+            this.pickImagen.TabIndex = 13;
+            this.pickImagen.TabStop = false;
+            // 
+            // btnAgregarImagen
+            // 
+            this.btnAgregarImagen.Location = new System.Drawing.Point(490, 244);
+            this.btnAgregarImagen.Name = "btnAgregarImagen";
+            this.btnAgregarImagen.Size = new System.Drawing.Size(71, 21);
+            this.btnAgregarImagen.TabIndex = 14;
+            this.btnAgregarImagen.Text = "agregar";
+            this.btnAgregarImagen.UseVisualStyleBackColor = true;
+            // 
+            // cboMensualidad
+            // 
+            this.cboMensualidad.FormattingEnabled = true;
+            this.cboMensualidad.Location = new System.Drawing.Point(97, 225);
+            this.cboMensualidad.Name = "cboMensualidad";
+            this.cboMensualidad.Size = new System.Drawing.Size(114, 21);
+            this.cboMensualidad.TabIndex = 15;
             // 
             // FormMenu
             // 
@@ -105,6 +207,7 @@
             this.Text = "FormMenu";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pickImagen)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -112,10 +215,20 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblMensualidad;
+        private System.Windows.Forms.Label lblDni;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblNombre;
         private System.Windows.Forms.Label lblClave;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.TextBox txtApelido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.Button btnAgregarImagen;
+        private System.Windows.Forms.PictureBox pickImagen;
+        private System.Windows.Forms.Label txtImagen;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button BtnReset;
+        private System.Windows.Forms.ComboBox cboMensualidad;
     }
 }
